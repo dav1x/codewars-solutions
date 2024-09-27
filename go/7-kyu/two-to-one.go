69fe7115e667902192341e558bd1f897ea325b9c
dphillip <dphillip@redhat.com>
2024-09-27 15:14:22 -0500

    two to one

---
+++ go/7-kyu/two-to-one.go
@@ -0,0 +1,12 @@
+package kata
+
+import "strings"
+
+func TwoToOne(s1 string, s2 string) string {
+  result := ""
+  for _, char := range strings.Split("abcdefghijklmnopqrstuvwxyz", "") {
+    if strings.Contains(s1+s2, char) {
+      result += char
+    }
+  }
+  return result
